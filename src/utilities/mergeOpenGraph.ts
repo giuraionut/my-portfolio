7a598fb199dd6973a260aa2f6e60ae83ee948e82
Giura Ionut <giuraionut98@gmail.com>
2025-12-27 14:46:50 +0200

    feat: update website references to 'Ionut Giura Website' across multiple files

--- src/utilities/mergeOpenGraph.ts
+++ src/utilities/mergeOpenGraph.ts
@@ -3,14 +3,14 @@ import { getServerSideURL } from './getURL'
 
 const defaultOpenGraph: Metadata['openGraph'] = {
   type: 'website',
-  description: 'An open-source website built with Payload and Next.js.',
+  description: 'Personal Portfolio built with Payload and Next.js.',
   images: [
     {
       url: `${getServerSideURL()}/website-template-OG.webp`,
     },
   ],
-  siteName: 'Payload Website Template',
-  title: 'Payload Website Template',
+  siteName: 'Ionut Giura Website',
+  title: 'Ionut Giura Website',
 }
 
 export const mergeOpenGraph = (og?: Metadata['openGraph']): Metadata['openGraph'] => {
